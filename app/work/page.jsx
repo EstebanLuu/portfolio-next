bc0b08a8773de106a429377394e56e6409837f3f
nicoluu <estebaanlunaaa@gmail.com>
2024-09-18 22:19:37 -0300

    feat: new portfolio

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -7,45 +7,46 @@ import { SiCss3, SiJavascript, SiSass, SiReact, SiRedux, SiNextdotjs, SiVite, Si
 import { BiLogoPostgresql } from "react-icons/bi";
 import { motion } from 'framer-motion'
 import Image from "next/image";
+import { v4 as uuidv4 } from 'uuid';
 
 const works = [
   {
-    key: 101,
+    key: uuidv4(),
     image: 'https://cdn.discordapp.com/attachments/1285951821944455230/1285968558026653726/image.png?ex=66ec330c&is=66eae18c&hm=a8037e2a27680ab076d6acb9d74c365c68ec14884f6e3ffb25f6b2bda2b797ce&',
     title: 'Gestini',
     description: 'ERP integral para la gestión de empresas, almacenes, usuarios y tienda en linea.',
     technologies: [<SiReact />, <SiSass />, <SiRedux />, <SiVite />, <SiNodedotjs />, <FaJava />, <SiMysql />]
   },
   {
-    key: 102,
+    key: uuidv4(),
     image: 'https://cdn.discordapp.com/attachments/1285951821944455230/1285968561025847378/image.png?ex=66ec330d&is=66eae18d&hm=293fdcef46f89ac7a2372609aff098781053b0dce0c4c48576dfcf3e3885b6f1&',
     title: 'Dream Junk Academy',
     description: 'Plataforma educativa con gestión de profesores, alumnos y clases en tiempo real.',
     technologies: [<SiSass />, <SiReact />, <SiVite />, <SiMongodb />, <SiNodedotjs />, <SiExpress />, <SiRedux />, <SiPostgresql />]
   },
   {
-    key: 103,
+    key: uuidv4(),
     image: 'https://cdn.discordapp.com/attachments/1285951821944455230/1285985076160499712/image.png?ex=66ec426e&is=66eaf0ee&hm=91f806a95df75959ec531422919a31389227596f48ad6cbe992b9fa60c111580&',
     title: 'Dream Junk Studio',
     description: 'Software para la gestión de proyectos, contratos y comunidad empresarial.',
     technologies: [<SiSass />, <SiReact />, <SiVite />, <SiMongodb />, <SiNodedotjs />, <SiExpress />, <SiRedux />, <SiPostgresql />]
   },
   {
-    key: 104,
+    key: uuidv4(),
     image: 'https://cdn.discordapp.com/attachments/1285951821944455230/1285968558748078080/image.png?ex=66ec330c&is=66eae18c&hm=54d50e91bc051e53f91cebec18b03fd46dbe08229ceebedfbbe6b9721fca8ebd&',
     title: 'Henry Shops',
     description: 'Herramienta para crear y personalizar plantillas web de manera sencilla.',
     technologies: [<SiSass />, <SiReact />, <SiVite />, <SiMongodb />, <SiNodedotjs />, <SiExpress />, <SiRedux />, <SiPostgresql />, <SiSequelize />]
   },
   {
-    key: 105,
+    key: uuidv4(),
     image: 'https://media.discordapp.net/attachments/1285951821944455230/1285968558307934230/image.png?ex=66ec330c&is=66eae18c&hm=5e6133f10fe88f79fbd4e171b37092efc045b912bf833217570d4b3f59e1757a&=&format=webp&quality=lossless&width=848&height=473',
     title: 'Merlok',
     description: 'Landing page profesional para una empresa de tecnología con diseño moderno.',
     technologies: [<SiSass />, <SiReact />, <SiNextdotjs />]
   },
   {
-    key: 106,
+    key: uuidv4(),
     image: 'https://cdn.discordapp.com/attachments/1285951821944455230/1286029118487396373/image.png?ex=66ec6b73&is=66eb19f3&hm=a99e6df6de66499a451c783863d466f8505a7172a172b34687df7b143088cdb3&',
     title: 'Conectando Empresas',
     description: 'Landing page con integración CRM Pipedrive para marketing.',
@@ -68,7 +69,7 @@ const Work = () => {
 
         <div className="container content py-5 xl:pb-10 grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
           {works.map((work, ind) => (
-            <div key={ind}>
+            <div key={work.key}>
               <CardContainer className="inter-var py-0">
                 <CardBody className="relative group/card hover:shadow-2xl hover:shadow-accent/[0.1] bg-[#ffffff0f] border-[#3ebcc031] w-auto sm:w-[20rem] h-auto rounded-xl p-6 border">
                   <CardItem
@@ -88,7 +89,7 @@ const Work = () => {
                     translateZ="100"
                     className="w-full mt-4 h-50 sm:h-40 overflow-hidden rounded-xl"
                   >
-                    <Image
+                    <img
                       src={work.image}
                       className="w-full object-contain rounded-xl  group-hover/card:shadow-xl"
                       alt="thumbnail"
@@ -97,7 +98,7 @@ const Work = () => {
                   <div className="flex justify-between items-center mt-5">
                     <CardItem className="flex gap-2" translateZ="60">
                       {work.technologies.map((icon, idx) => (
-                        <span key={idx} className="text-base text-white shadow-xl">
+                        <span key={`${work.key}${idx}`} className="text-base text-white shadow-xl">
                           {icon}
                         </span>
                       ))}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['cdn.discordapp.com', 'media.discordapp.net'],
+    },
+  }
 
 export default nextConfig;
