0cfb935638e427f938de47d46cdad0c36fb30423
nicoluu <estebaanlunaaa@gmail.com>
2024-09-20 18:18:31 -0300

    feat: new commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['cdn.discordapp.com', 'media.discordapp.net'],
-  },
-}
+const nextConfig = {}
 
 export default nextConfig;
